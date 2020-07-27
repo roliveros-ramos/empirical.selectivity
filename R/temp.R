bddc8a8d593b6cf4484c7b8dcd9bd2eb8b6c4c23
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-07-27 06:22:02 -0700

    temporary borrowed functions.

---
+++ R/temp.R
@@ -0,0 +1,33 @@
+#' Directional color palette.
+#'
+#' @param n Number of colors
+#' @param col Base color for the palette.
+#' @param p Power to scale the increase in intensity. Default is 0.8, 1 gives linear decrease.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+directionalPalette = function(n=64, col="green3", p=0.8, x=NULL, ...) {
+
+  alpha = seq(from=0, to=1, length=n)^p
+
+  cols = sapply(alpha, FUN=.makeTransparent, col=col)
+
+  if(!is.null(x)) {
+    ind = cut(x, breaks = n, labels = FALSE)
+    return(cols[ind])
+  }
+
+  return(unlist(cols))
+
+}
+
+
+# Internal ----------------------------------------------------------------
+
+.makeTransparent = function(alpha, col) {
+  col = col2rgb(col)
+  alpha = floor(255*alpha)
+  rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+}
