83266c11cb5a5252acb65378da490bc4d8e05f80
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-08-26 12:48:42 -0700

    .getBlocks uses method now, only "equal" currently in production.

--- R/empirical_selectivity-internal.R
+++ R/empirical_selectivity-internal.R
@@ -176,16 +176,25 @@
 }
 
 
-.getBlockBreaks = function(object, n) {
+.getBlockBreaks = function(object, n, method="equal") {
 
   if(is.null(attr(object, "weights")))
     stop("No time dimension available for computing blocks.")
 
   yr = attr(object, "weights")$year
 
-  breaks = ceiling(quantile(yr, prob=seq(0, 1, len=n+1)))
+  if(method=="equal") {
+    breaks = ceiling(quantile(yr, prob=seq(0, 1, len=n+1)))
+    return(breaks)
+  }
+
+  # method using clustering
+
+}
+
+.optimBlocks = function(object, breaks, w) {
+
 
   return(breaks)
 
 }
-
