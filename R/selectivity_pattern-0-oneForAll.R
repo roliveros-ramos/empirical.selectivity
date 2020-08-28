f32b50d8317c9ca4733808036bc6beb54d3ce36c
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-08-27 19:17:44 -0700

    pattern=0 (one for all) is now the core of the fitting.

--- R/selectivity_pattern-0-oneForAll.R
+++ R/selectivity_pattern-0-oneForAll.R
@@ -2,8 +2,9 @@
 fit_selectivity_0 = function(object, pattern, ...) {
   # create a list of model parameters
 
-  out = list()
-  fit = list()
+  out  = list()
+  fit  = list()
+  npar = list()
 
   for(patt in pattern) {
     nm = as.character(patt)
@@ -11,31 +12,36 @@ fit_selectivity_0 = function(object, pattern, ...) {
     FUN = match.fun(sprintf("fit_selectivity_%s", nm))
     outx  = FUN(object, ...)
     out[[nm]] = outx
-    fit[[nm]] = .compare_fit(outx)
+    fit[[nm]] = outx$fit
+    npar[[nm]] = outx$npar
   }
 
-  fit = as.data.frame(fit)
+  x = as.numeric(colnames(object))
+
+  fit = as.data.frame(fit, check.names=FALSE)
+  npar = as.data.frame(npar, check.names=FALSE)
 
   best_model = apply(fit, 1, which.min)
   names(best_model) = rownames(object)
 
   xo = out[[1]]$selectivity
   output = out[[1]]$models
-  best_pattern = numeric(length(best_model))
+  best_pattern = out[[1]]$pattern
+  names(best_pattern) = rownames(object)
 
-  message("Best models:\n")
-
-  for(i in seq_along(best_model)) {
-    this = out[[best_model[i]]]
-    xo[i, ] = as.numeric(this$selectivity[i, ])
-    output[[i]] = this$models[[i]]
-    best_pattern[i] = this$pattern[i]
-    message(names(best_model)[i], ": ", this$pattern[i])
+  if(length(pattern)>1) {
+    message("Best models:\n")
+    for(i in seq_along(best_model)) {
+      this = out[[best_model[i]]]
+      xo[i, ] = as.numeric(this$selectivity[i, ])
+      output[[i]] = this$models[[i]]
+      best_pattern[i] = this$pattern[i]
+      message(names(best_model)[i], ": ", this$pattern[i])
+    }
   }
 
-  names(best_pattern) = names(best_model)
-
-  output = list(selectivity=xo, models=output, y=object, x=x, pattern=best_pattern)
+  output = list(selectivity=xo, models=output, y=object, x=x, pattern=best_pattern,
+                fit=fit, npar=npar)
 
   return(output)
 
@@ -45,10 +51,18 @@ fit_selectivity_0 = function(object, pattern, ...) {
 # Internal functions ------------------------------------------------------
 
 
-.compare_fit = function(object) {
-  fit = object$selectivity
-  y = object$y
+# .compare_fit = function(object) {
+#   fit = object$selectivity
+#   y = object$y
+#   out = rowSums((log(fit) - log(y))^2)
+#   return(out)
+# }
+
+.compare_fit = function(fit, y) {
   out = rowSums((log(fit) - log(y))^2)
   return(out)
 }
+
+
+
 
