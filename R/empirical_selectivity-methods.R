e72da73a7f65c6c69ddfea47f116331bd5ce2fe2
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-09-10 13:56:17 -0700

    splitting files for pull request to r4ss

---
+++ R/empirical_selectivity-methods.R
@@ -0,0 +1,149 @@
+# Methods for empirical_selectivity ---------------------------------------
+
+#' Title
+#' @param x
+#'
+#' @param w
+#' @param ...
+#'
+#' @export
+weighted.mean.empirical_selectivity = function(x, w, ...) {
+
+  if(is.character(w)) {
+    w = match.arg(w, c("catch", "Neff", "Nsamp", "equal"))
+    lab = sprintf("%s (%s weighted)", attr(x, "fleet"), w)
+    w = if(w=="equal") 1 else attr(x, "weights")[, w]
+  } else lab = attr(x, "fleet")
+
+  if(any(is.na(w))) stop("NA in weights are not allowed.")
+
+  N = nrow(x)
+  if(length(w)==1) w = rep(w, N)
+  if(length(w)!=N) stop("One weight for each x row must be indicated.")
+
+  rs = rowSums(x)
+  w[rs==0] = 0 # rows with zero values get 0 weight
+  w = w/sum(w) # weigths normalized to sum 1.
+
+  output = colSums(x*w)
+  output = output/max(output, na.rm=TRUE) # max to 1.
+  output = matrix(output, nrow=1)
+  colnames(output) = colnames(x)
+  rownames(output) = lab
+
+  attr(output, "fleet") = attr(x, "fleet")
+  attr(output, "by") = attr(x, "by")
+  xmod = attr(x, "model")
+  xmod$y = matrix(colSums(w*xmod$y), nrow=1)
+  attr(output, "model") = xmod
+
+  class(output) = c("empirical_selectivity", "SS_timexsize", "matrix")
+  return(output)
+
+}
+
+#' @export
+weighted.mean.SS_empirical_selectivity = function(x, w, ...) {
+
+  output = lapply(x, FUN=weighted.mean, w=w)
+  class(output) = "SS_empirical_selectivity"
+  return(output)
+
+}
+
+#' @export
+weighted.mean.list = function(x, w, ...) {
+
+  output = lapply(x, FUN=weighted.mean, w=w)
+  return(output)
+
+}
+
+
+#' Title
+#' @param x
+#'
+#' @param i
+#' @param j
+#' @param yr
+#' @param age
+#' @param length
+#' @param drop
+#'
+#' @export
+'[.empirical_selectivity' = function(x, i, j, yr, age, length, drop = FALSE) {
+
+  attList = attributes(x)
+  class(x) =  "matrix"
+
+  if(!missing(yr)) {
+    years = suppressWarnings(as.numeric(rownames(x)))
+    if(all(is.na(years))) stop("No year data is available for indexing.")
+    i = na.omit(match(yr, years))
+  }
+
+  if(!missing(age)) {
+    if(attr(x, "by")!="age") stop("No age data is available for indexing.")
+    ages = suppressWarnings(as.numeric(colnames(x)))
+    # extract ages withing the range
+    j = which(ages >= min(age, na.rm=TRUE) & ages <= max(age, na.rm=TRUE))
+  }
+
+  if(!missing(length)) {
+    if(attr(x, "by")!="length") stop("No length data is available for indexing.")
+    lengths = suppressWarnings(as.numeric(colnames(x)))
+    # extract lengths withing the range
+    j = which(lengths >= min(length, na.rm=TRUE) & lengths <= max(length, na.rm=TRUE))
+  }
+
+  out = '['(x, i, j, drop=drop)
+  class(out) = c("empirical_selectivity", "SS_timexsize", "matrix")
+
+  attr(out, "by") = attList$by
+  attr(out, "fleet") = attList$fleet
+
+  attList$model$y = attList$model$y[i, ]
+  attList$model$years = attList$model$years[i]
+
+  attr(out, "model") = attList$model
+  if(!is.null(attList$weights)) attr(out, "weights") = attList$weights[i,]
+
+  return(out)
+}
+
+
+#' Title
+#' @param x
+#'
+#' @param f
+#' @param breaks
+#' @param drop
+#' @param sep
+#' @param lex.order
+#' @param ...
+#'
+#' @export
+split.empirical_selectivity = function (x, f, breaks, drop = FALSE, sep = ".",
+                                        lex.order = FALSE, ...) {
+
+  if(!missing(breaks)) {
+    if(is.null(attr(x, "weights")))
+      stop("No time dimension available for computing blocks.")
+    yr = attr(x, "weights")$year
+    f = cut(yr, breaks = breaks, right=FALSE, include.lowest = TRUE)
+  }
+
+  if (is.list(f))
+    f = interaction(f, drop = drop, sep = sep, lex.order = lex.order)
+  else if(!is.factor(f))
+    f = as.factor(f)
+  else if(drop)
+    f = factor(f)
+  storage.mode(f) = "integer"
+  ind = .Internal(split(seq_len(nrow(x)), f))
+  out = lapply(ind, function(i) x[i])
+  return(out)
+
+}
+
+
--- R/empirical_selectivity.R
+++ R/empirical_selectivity.R
@@ -64,152 +64,190 @@ empirical_selectivity.SS_output = function(object, fleet=NULL, sex=1, by="length
 }
 
 
-# Methods for empirical_selectivity ---------------------------------------
-
-#' Title
-#' @param x
-#'
-#' @param w
-#' @param ...
-#'
-#' @export
-weighted.mean.empirical_selectivity = function(x, w, ...) {
-
-  if(is.character(w)) {
-    w = match.arg(w, c("catch", "Neff", "Nsamp", "equal"))
-    lab = sprintf("%s (%s weighted)", attr(x, "fleet"), w)
-    w = if(w=="equal") 1 else attr(x, "weights")[, w]
-  } else lab = attr(x, "fleet")
-
-  if(any(is.na(w))) stop("NA in weights are not allowed.")
-
-  N = nrow(x)
-  if(length(w)==1) w = rep(w, N)
-  if(length(w)!=N) stop("One weight for each x row must be indicated.")
-
-  rs = rowSums(x)
-  w[rs==0] = 0 # rows with zero values get 0 weight
-  w = w/sum(w) # weigths normalized to sum 1.
-
-  output = colSums(x*w)
-  output = output/max(output, na.rm=TRUE) # max to 1.
-  output = matrix(output, nrow=1)
-  colnames(output) = colnames(x)
-  rownames(output) = lab
-
-  attr(output, "fleet") = attr(x, "fleet")
-  attr(output, "by") = attr(x, "by")
-  xmod = attr(x, "model")
-  xmod$y = matrix(colSums(w*xmod$y), nrow=1)
-  attr(output, "model") = xmod
-
-  class(output) = c("empirical_selectivity", "SS_timexsize", "matrix")
-  return(output)
-
-}
-
-#' @export
-weighted.mean.SS_empirical_selectivity = function(x, w, ...) {
-
-  output = lapply(x, FUN=weighted.mean, w=w)
-  class(output) = "SS_empirical_selectivity"
-  return(output)
-
-}
-
-#' @export
-weighted.mean.list = function(x, w, ...) {
-
-  output = lapply(x, FUN=weighted.mean, w=w)
-  return(output)
-
-}
+# Internal functions ------------------------------------------------------
 
 
-# Methods -----------------------------------------------------------------
+.mta = function(x, bins, years, var="Obs", FUN=mean, scale=1, by="length",
+                mid = NULL, normalize=TRUE, bin.name = "Bin", thr=1e-5) {
 
-#' Title
-#' @param x
-#'
-#' @param i
-#' @param j
-#' @param yr
-#' @param age
-#' @param length
-#' @param drop
-#'
-#' @export
-'[.empirical_selectivity' = function(x, i, j, yr, age, length, drop = FALSE) {
+  FUN = match.fun(FUN)
+  out = matrix(0, nrow=length(years), ncol=length(bins))
+  rownames(out) = years
+  colnames(out) = bins
 
-  attList = attributes(x)
-  class(x) =  "matrix"
+  msg = sprintf("Variable %s not found.", var)
+  if(!(var %in% names(x))) stop(msg)
 
-  if(!missing(yr)) {
-    years = suppressWarnings(as.numeric(rownames(x)))
-    if(all(is.na(years))) stop("No year data is available for indexing.")
-    i = na.omit(match(yr, years))
+  xout  = tapply(X = x[, var], INDEX=list(x$Yr, x[, bin.name]), FUN=FUN)
+
+  obins = as.numeric(colnames(xout))
+  oyear = as.numeric(rownames(xout))
+
+  rrows = match(oyear, years)
+  rcols = match(obins, bins)
+
+  out[na.omit(rrows), na.omit(rcols)] = xout[!is.na(rrows), !is.na(rcols)]
+
+  if(is.null(mid)) colnames(out) = mid
+
+  if(normalize) {
+
+    if(is.matrix(scale)) {
+      # convert scale to proportion
+      scale = scale/rowSums(scale, na.rm=TRUE)
+      ind = which(colSums(scale>thr) == 0)
+      scale[, ind] = NA
+    }
+
+    out = out/scale
+    xmax = apply(out, 1, max, na.rm=TRUE)
+    xmax[xmax==0] = 1
+    out = out/xmax
+
+    out[is.nan(out)] = 0
+    out[, ind] = NA
+
+
+  } else {
+    out = out/scale
+    out[is.nan(out)] = 0
+    out[is.na(out)]  = 0
+    names(dimnames(out)) = c("year", by)
+    return(out)
   }
 
-  if(!missing(age)) {
-    if(attr(x, "by")!="age") stop("No age data is available for indexing.")
-    ages = suppressWarnings(as.numeric(colnames(x)))
-    # extract ages withing the range
-    j = which(ages >= min(age, na.rm=TRUE) & ages <= max(age, na.rm=TRUE))
-  }
+  names(dimnames(out)) = c("year", by)
+  attr(out, "by") = by
 
-  if(!missing(length)) {
-    if(attr(x, "by")!="length") stop("No length data is available for indexing.")
-    lengths = suppressWarnings(as.numeric(colnames(x)))
-    # extract lengths withing the range
-    j = which(lengths >= min(length, na.rm=TRUE) & lengths <= max(length, na.rm=TRUE))
-  }
-
-  out = '['(x, i, j, drop=drop)
-  class(out) = c("empirical_selectivity", "SS_timexsize", "matrix")
-
-  attr(out, "by") = attList$by
-  attr(out, "fleet") = attList$fleet
-
-  attList$model$y = attList$model$y[i, ]
-  attList$model$years = attList$model$years[i]
-
-  attr(out, "model") = attList$model
-  if(!is.null(attList$weights)) attr(out, "weights") = attList$weights[i,]
+  if(by %in% c("length", "age"))
+    class(out) = c("empirical_selectivity", "SS_timexsize", "matrix")
 
   return(out)
+
+}
+
+.getES = function(object, by="length", use="B", thr=1e-5) {
+
+  by = match.arg(by, choices = c("length", "age"))
+
+  db  = sprintf("%sdbase", substr(by, 1, 3))
+  nat = sprintf("nat%s", substr(by, 1, 3))
+  years = seq(from=object$startyr, to=object$endyr, by=1)
+
+  naged  = object[[db]]
+  natage = object[[nat]]
+
+  nsamp = c("Nsamp_adj", "Nsamp_in", "Nsamp", "N")
+  nsamp = nsamp[ min(which(nsamp %in% names(naged)))]
+
+  # fill and zeros
+  w_catch = .mta(object$catch, bins=seq_along(object$FleetNames),
+                 years=years, var="Obs", FUN=mean, by="fleet", normalize=FALSE, bin.name = "Fleet")
+  w_nsamp = .mta(naged, bins=seq_along(object$FleetNames),
+                 years=years, var=nsamp, FUN=mean, by="fleet", normalize=FALSE, bin.name = "Fleet")
+  w_effN  = .mta(naged, bins=seq_along(object$FleetNames),
+                 years=years, var="effN", FUN=mean, by="fleet", normalize=FALSE, bin.name = "Fleet")
+
+  thisFleets = unique(naged$Fleet)
+
+  popbin = as.numeric(colnames(natage)[-(1:12)])
+  fshbin = if(by=="age") object$agebins else object$lbins
+
+  if(all(is.na(fshbin))) fshbin = popbin
+
+  bins = .checkBins(fshbin = fshbin, popbin = popbin)
+
+  natage = natage[natage$'Beg/Mid'==use & natage$Era=="TIME", ]
+  natage = natage[order(natage$Yr), ]
+
+  nbase = expand.grid(Yr=years, Sex=c(1,2))
+  nbase = merge(nbase, natage, all=TRUE)
+  nbase = nbase[order(nbase$Yr), ]
+
+  yr = sort(unique(natage$Yr))
+  natage_1 = natage[natage$Sex==1, -(1:12)]
+  natage_2 = natage[natage$Sex==2, -(1:12)]
+
+  natage = as.matrix(natage_1) + as.matrix(natage_2)
+  natage = natage %*% bins$conv # reshape to new bins
+
+  output = lapply(split(naged, f = list(naged$Fleet, naged$Sex)),
+                  FUN = .mta, bins=bins$bin, years=years, scale=natage, by=by,
+                  mid = bins$mid, thr=thr)
+
+  for(i in seq_along(output)) {
+    # thisFleets has the "fleet code"
+    attr(output[[i]], "fleet") = object$FleetNames[thisFleets[i]]
+    attr(output[[i]], "weights") = data.frame(year  = years,
+                                              catch = w_catch[, thisFleets[i]],
+                                              Nsamp = w_nsamp[, thisFleets[i]],
+                                              Neff  = w_effN[, thisFleets[i]])
+    sex = as.numeric(strsplit(names(output)[i], "\\.")[[1]][2])
+    attr(output[[i]], "model") = .getSelex(object, fleet = thisFleets[i], sex=sex , by=by)
+
+  }
+
+  return(output)
+
+}
+
+.checkBins = function(fshbin, popbin, n=20, k=1000) {
+
+  find0 = seq_len(length(fshbin)+2*n) - n
+  fmod = lm(bin ~ ind, data=data.frame(bin=fshbin, ind=seq_along(fshbin)))
+
+  pind0 = seq_len(length(popbin)+1)
+  pmod = lm(bin ~ ind, data=data.frame(bin=popbin, ind=seq_along(popbin)))
+
+  # nbin extends fshbins
+  nbin0 = round(as.numeric(predict(fmod, newdata = data.frame(ind=find0))),3)
+  mid0 = as.numeric(filter(nbin0, filter=c(0.5, 0.5), side=1))
+  ind1 = which(nbin0 >= min(popbin) & nbin0 <= max(popbin))
+  nbin = nbin0[ind1]
+  mid = mid0[ind1+1]
+  top = nbin0[ind1+1]
+  pbin = popbin[popbin >= min(nbin) & popbin <= max(nbin)]
+
+  cond = identical(nbin, pbin)
+
+  popbin2 = round(as.numeric(predict(pmod, newdata = data.frame(ind=pind0))),3)
+
+  # if cond is FALSE, regrid popbin
+
+  mini = head(approx(x=popbin2, n=k*length(popbin2)-(k-1))$y, -1)
+
+  out = cut(mini, breaks=c(nbin, max(top)), labels=FALSE, right = FALSE)
+  levels = unique(na.omit(out))
+  out = matrix(out, ncol=k, byrow = TRUE)
+  .mytable = function(x, levels) table(factor(x, levels=levels))
+  xout = t(apply(out, 1, .mytable, levels=levels))/k
+
+  output = list(bin=nbin, mid=mid, conv = xout)
+  return(output)
 }
 
 
-#' Title
-#' @param x
-#'
-#' @param f
-#' @param breaks
-#' @param drop
-#' @param sep
-#' @param lex.order
-#' @param ...
-#'
-#' @export
-split.empirical_selectivity = function (x, f, breaks, drop = FALSE, sep = ".",
-                                        lex.order = FALSE, ...) {
+.getSelex = function(object, fleet, sex=1, by="length") {
 
-  if(!missing(breaks)) {
-    if(is.null(attr(x, "weights")))
-      stop("No time dimension available for computing blocks.")
-    yr = attr(x, "weights")$year
-    f = cut(yr, breaks = breaks, right=FALSE, include.lowest = TRUE)
-  }
+  obj = if(by=="length") object$sizeselex else object$ageselex
+  Asel = ifelse("Asel" %in% names(table(object$ageselex$Factor)), "Asel", "Asel2")
+  var = if(by=="length") "Lsel" else Asel
+  n = if(by=="length") 5 else 7
 
-  if (is.list(f))
-    f = interaction(f, drop = drop, sep = sep, lex.order = lex.order)
-  else if(!is.factor(f))
-    f = as.factor(f)
-  else if(drop)
-    f = factor(f)
-  storage.mode(f) = "integer"
-  ind = .Internal(split(seq_len(nrow(x)), f))
-  out = lapply(ind, function(i) x[i])
+  tt = obj[obj$Factor %in% var & obj$Fleet %in% fleet & obj$Sex %in% sex, ]
+  syr = tt$Yr
+  years = seq(from=object$startyr, to=object$endyr, by=1)
+
+  tt = cbind(Yrf=cut(syr, breaks=syr, right = FALSE, include.lowest = TRUE), tt[, -(1:n)])
+
+  db = data.frame(Yr=years, Yrf=cut(years, breaks=syr, right = FALSE, include.lowest = TRUE))
+
+  db = merge(db, tt, sort=FALSE, all.y=TRUE)
+  db = db[!is.na(db$Yr), , drop=FALSE]
+  db = db[order(db$Yr), -c(1,2), drop=FALSE]
+
+  out = list(x=as.numeric(colnames(db)), y=as.matrix(db), years=years)
+
   return(out)
 
 }
