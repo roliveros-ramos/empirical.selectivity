6adc943a984addf73ccf884446a30edd50c02064
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2020-09-11 00:18:40 -0700

    Defining imports to avoind clushing with tidyverse packages.

---
+++ R/aaa-.R
@@ -0,0 +1,16 @@
+#' @importFrom stats optim weighted.mean
+NULL
+#' @importFrom grDevices col2rgb rgb
+NULL
+#' @importFrom graphics abline axis box legend lines par plot.new
+#' plot.window points title
+NULL
+#' @importFrom stats approx filter lm median na.omit predict quantile
+NULL
+#' @importFrom utils head tail
+NULL
+#' @importFrom fields image.plot
+NULL
+#' @import r4ss
+NULL
+
