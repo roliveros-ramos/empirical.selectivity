e4e1dd6ce03375e6faf2a3ec09adf72ddca38689
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2021-05-28 23:39:29 -0500

    Changes in logistic function: includes new parameter "prob" with default to 95%, but can be used to estimate any other (like 75%). Also adds third parameter for scale.

--- R/selectivity_pattern-1-logistic.R
+++ R/selectivity_pattern-1-logistic.R
@@ -1,44 +1,50 @@
 
-fit_selectivity_1 = function(object, FUN, ...) {
+fit_selectivity_1 = function(object, prob=0.95, FUN, ...) {
   # create a list of model parameters
 
   FUN = match.fun(FUN)
 
   # main function to be applied to 'empirical_selectivity' object
-  .fit_selectivity_1 = function(x, y, ...) {
+  .fit_selectivity_1 = function(x, y, prob, ...) {
 
     x = as.numeric(x)
     y = as.numeric(y)
-    par = .logistic1_guess(x, y)
-    mod = optim(par, fn=.logistic1_fit, x=x, y=y, FUN=FUN, method="L-BFGS-B")
+    par = .logistic1_guess(x, y, prob=prob)
+    mod = optim(par, fn=.logistic1_fit, x=x, y=y, prob=prob, FUN=FUN, method="L-BFGS-B",
+                lower=c(0.1, 0.1, 0.5), upper=c(Inf, Inf, 2))
     mod$guess = par
-    pred = .logistic1(x, mod$par)
+    pred = .logistic1(x, mod$par, prob=prob)
+    scale = mod$par[3]
 
-    output = list(fitted=pred, x=x, y=y, model=mod, npar=2)
+    output = list(fitted=pred, x=x, y=y, model=mod, npar=2, scale=scale)
     return(output)
   }
 
   x = as.numeric(colnames(object))
 
   xo = object*0
+  yo = object*0
   out = vector("list", nrow(object))
   npar = vector("integer", nrow(object))
+  scales = vector("double", nrow(object))
 
   labs = sprintf("year = %s", rownames(object))
   if(nrow(object)==1) labs = rownames(object)
 
   for(i in seq_len(nrow(object))) {
     message("Fitting ", labs[i], "\n")
-    tmp = .fit_selectivity_1(x=x, y=object[i, ], ...)
-    out[[i]] = tmp$model
-    xo[i, ]  = tmp$fitted
-    npar[i]  = tmp$npar
+    tmp = .fit_selectivity_1(x=x, y=object[i, ], prob=prob, ...)
+    out[[i]]  = tmp$model
+    xo[i, ]   = tmp$fitted
+    yo[i, ]   = tmp$y
+    npar[i]   = tmp$npar
+    scales[i] = tmp$scale
   }
 
   fit = .compare_fit(xo, object, FUN)
 
-  output = list(selectivity=xo, models=out, y=object, x=x, pattern=rep(1, nrow(xo)),
-                fit=fit, npar=npar)
+  output = list(selectivity=xo, models=out, y=yo, x=x, pattern=rep(1, nrow(xo)),
+                fit=fit, npar=npar, scale=scales)
 
   return(output)
 }
@@ -46,35 +52,44 @@ fit_selectivity_1 = function(object, FUN, ...) {
 
 # Internal functions ------------------------------------------------------
 
-.logistic1 = function(x, par) {
+.logistic1 = function(x, par, prob=0.95) {
 
   p1 = par[1]
   p2 = par[2]
+  p3 = par[3]
 
-  sel = 1/(1 + exp(-1*log(19)*(x-p1)/p2))
+  const = 1/(1 - prob) - 1
+
+  sel = 1/(1 + exp(-1*log(const)*(x-p1)/p2))
 
   return(sel)
 }
 
-.logistic1_guess = function(x, y) {
 
-  p1 = min(x[which(y>=0.5)])
-  p2 = min(x[which(y>=0.95)]) - p1
+.logistic1_guess = function(x, y, prob=0.95) {
 
-  out = c(p1=p1, p2=p2)
+  p1 = min(x[which(y >= 0.5)])
+  p2 = min(x[which(y >= prob)]) - p1
+  top = y[which(y >= 0.95)]
+  p3 = mean(top, na.rm=TRUE)/max(top, na.rm=TRUE)
+
+  if(is.nan(p3)) p3 = 1
+  if(!is.finite(p3)) p3 = 1
+
+  out = c(p1=p1, p2=p2, p3=p3)
 
   return(out)
 
 }
 
-.logistic1_fit = function(par, x, y, FUN) {
+.logistic1_fit = function(par, x, y, FUN, prob=0.95) {
   FUN = match.fun(FUN)
   removeZero = !is.finite(FUN(1,0))
   if(removeZero) {
     tinyExp = floor(log10(min(y[y!=0]))) - 3 # removeZeros
     y[y==0] = 10^tinyExp
   }
-  fit = .logistic1(x, par)
+  fit = par[3]*.logistic1(x, par, prob=prob)
   out = sum(FUN(fit, y), na.rm=TRUE)
   return(out)
 }
@@ -98,7 +113,7 @@ fit_selectivity_1 = function(object, FUN, ...) {
 
   lo   = c(m, -(M-m))
   hi   = c(M, M-m)
-  init = object$models[[t]]$par
+  init = object$models[[t]]$par[1:2]
   init = pmax(pmin(init, hi), lo)
   init = round(init, 3)
   prior = c(round(0.5*(m+M)), 0)
